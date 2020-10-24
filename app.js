0c4c39c349c3800f9cb5381636c9227997a03ca4
Kurikania <65830166+Kurikania@users.noreply.github.com>
2020-10-24 09:38:49 +0300

    registration routes

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
+
+
 const express = require("express"); 
 const app = express(); 
 const ejs = require("ejs");
 const bodyParser = require("body-parser"); 
 const path = require("path"); 
 const methodOverride = require("method-override");
-const mongoose = require("mongoose"); 
-const User = require("./models/user"); 
 const passport = require("passport"); 
-const LocalStrategy  = require("passport-local"); 
+const bcrypt = require("bcrypt"); 
+const flash = require('express-flash'); 
+const session = require('express-session')
+const initializePassport = require('./passport-config')
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
+
 
 const PORT = process.env.PORT || 5000;
 
@@ -15,32 +24,36 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '/public')));
 
-mongoose.connect("mongodb://localhost/auth", {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true });
+
+
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(methodOverride('_method'));
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(require("express-session")({
-    secret: "I never had a hamster",
-    resave: false,
-    saveUninitialized: false
-}));
 
-app.use(passport.initialize());
-app.use(passport.session());
 
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+let users = []; 
+
+
+
+app.use(flash())
+app.use(session({
+  secret: "String",
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+
 
 
 app.get("/", (req, res) => {
     res.render("index")
 })
 
-app.get("/index", (req, res) => {
+app.get("/index",   (req, res) => {
     res.render("index")
 })
 
@@ -48,51 +61,62 @@ app.get("/about", (req, res) => {
     res.render("about")
 })
 
-app.get("/loggedin", isLoggedIn,function(req, res){
-	res.render("loggedin");
-})
 
+app.get("/register", checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs')
+  })
 
-app.get("/register", (req, res) => {
-	res.render("register");
-})
+app.post('/register', async (req, res) => { 
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10); 
+        users.push({
+            id: Date.now.toString(), 
+            name: req.body.username, 
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect("/login")
+    } catch {
+        res.redirect("/register")
+    }
+    console.log(users)
+});
 
-app.post("/register", (req, res) => {
-	User.register(new User({username: req.body.username}), req.body.password, function(err, user){
-		if (err) {
-			console.log(err); 
-			res.render("register"); 
-		} 
-		passport.authenticate("local")(req, res, function(){
-			res.redirect("/loggedin");
-		});
-	})
-})
 
 app.get("/login", (req, res) => {
 	res.render("login"); 
 }); 
 
-app.post("/login", passport.authenticate("local", {
-	successRedirect: "/loggedin", 
-	failureRedirect: "/login"
-}), function(req,res){ });
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/loggedin',
+    failureRedirect: '/login',
+    failureFlash: true
+  }))
 
-app.post("/login", passport.authenticate("local", {
-	successRedirect: "/loggedin", 
-	failureRedirect: "/login"
-}), function(req,res){ });
-
-app.get("/logout", (req, res) => {
-	req.logout();
-	res.redirect("/");
+  app.get("/loggedin", checkAuthenticated, (req, res) => {
+	res.render("loggedin", { name: req.user.name });
 })
 
-function isLoggedIn(req, res, next){
-	if (req.isAuthenticated()){
-		return next();
-	}
-	res.redirect("/login")
-}
+  app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
+  })
+
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    return res.redirect('/loggedin')
+  }
+  
+  function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/')
+    }
+    next()
+  }
+  
+
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
